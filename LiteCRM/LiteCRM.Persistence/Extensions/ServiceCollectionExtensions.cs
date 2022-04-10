66633b6ebfd356eace5addcfa1b1fdaafb1e7fa6
Nikita Ivanov <keksualni.work@gmail.com>
2022-04-10 11:32:52 +0300

    Implement persistence extension

--- LiteCRM/LiteCRM.Persistence/Extensions/ServiceCollectionExtensions.cs
+++ LiteCRM/LiteCRM.Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using LiteCRM.Application.Contracts;
+using LiteCRM.Persistence.Options;
+using LiteCRM.Persistence.Repositories;
 using Microsoft.Extensions.DependencyInjection;
+using Raven.DependencyInjection;
 
 namespace LiteCRM.Persistence.Extensions;
 
@@ -7,8 +10,27 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddPersistence(
         this IServiceCollection services,
-        IConfiguration configuration)
+        Action<DocumentStoreOptions> bind)
     {
+        var options = new DocumentStoreOptions();
+        bind(options);
+
+        if (options.Url is null || options.Database is null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        services.AddRavenDbDocStore(ravenOptions =>
+        {
+            ravenOptions.Settings!.Urls = new[] {options.Url};
+            ravenOptions.Settings.DatabaseName = options.Database;
+        });
+        services.AddRavenDbAsyncSession();
+
+        services.AddScoped<IApplicationRepository, ApplicationRepository>();
+        services.AddScoped<IStoreItemRepository, StoreItemRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
         return services;
     }
 }
---
+++ LiteCRM/LiteCRM.Persistence/Options/DocumentStoreOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace LiteCRM.Persistence.Options;
+
+public class DocumentStoreOptions
+{
+    public string Url { get; set; }
+
+    public string Database { get; set; }
+}
